c249ced2e50f163fecdd84fb26d335271e776b30
17020928 <43131375+17020928@users.noreply.github.com>
2018-09-14 09:21:32 +0700

    dsfsdfsdf

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -2,37 +2,56 @@ package week1;
 
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
-
+    private String name,id,group,email;
     // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
+	// Tên
+	public void setname(String name){
+		this.name = name;
+	}
+	public String getname(){
+		return name;	
+	}
+	//Id
+	public void setid(String id){
+		this.id = id;
+	}
+	public String getId(){
+		return id;	
+	}
+	//Group
+	public void setgroup(String group){
+		this.group = group;
+	}
+	public String getgroup(){
+		return group;	
+	}
+	//email
+	public void setemail(String email){
+		this.email = email;
+	}
+	public String getemail() { return name; }
     Student() {
-        // TODO:
-    }
-
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
+		// TODO:
+		name = "Student";
+		id = "000";
+		group = "INT22401";
+		email = "uet@vnu.edu.vn";
+	}
     Student(String n, String sid, String em) {
         // TODO:
+		name=n;
+		id=sid;
+		email=em;
+		group="INT22401";
     }
-
-    /**
-     * Constructor 3
-     * @param s
-     */
     Student(Student s) {
         // TODO:
+		name=s.name;
+		id=s.id;
+		group=s.group;
+		email=s.email;
     }
-
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
-    }
+	public String getInfo(){
+		return(name+"\t"+id+"\t"+group+"\t"+email);
+	}
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,21 +3,35 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+	Student[] students =new Student[100];
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+		if(s1.getgroup()==s2.getgroup()) return true;
+        return false; 
     }
-
+	
     void studentsByGroup() {
         // TODO:
-    }
-
-    void removeStudent(String id) {
+		for(int i=0;i<100;i++){
+			if(students[i].getname() != "")
+			System.out.println(students[i].getInfo());
+		}
+	}
+	void removeStudent(String id) {
         // TODO:
-    }
-
+		for(int i=0;i<100;i++){
+			if(students[i].getId()==id) students[i].setid("");
+	}
+	}
     public static void main(String[] args) {
         // TODO:
-    }
+		StudentManagement st= new StudentManagement();
+		/*st.students[0]=new Student("Nghia","17020928","trandainghia@gmail.com");
+		st.students[1]=new Student("Nhanh","17020942","nguyenngocnhanh@gmail.com");
+		st.students[2]=new Student("Hau","170209234","nguyenthihau@gmail.com");
+		st.removeStudent("17020928");
+		st.studentsByGroup();
+		if(st.sameGroup(st.students[0],st.students[1])) System.out.println("asdasd");
+		*/
+	}
 }
