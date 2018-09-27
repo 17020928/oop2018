516275bc71ceadb10c72276c33e7f5a05a02a45a
17020928 <43131375+17020928@users.noreply.github.com>
2018-09-27 22:35:10 +0700

    change

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,10 +1,11 @@
 package week3;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Week3Test {
 
     // TODO: Viết 5 testcase cho phương thức max()
+    @Test
     public void testMax(){
         assertEquals(6,Week3.max(2,6));
         assertEquals(7,Week3.max(4,7));
@@ -12,19 +13,19 @@ public class Week3Test {
         assertEquals(180,Week3.max(32,180));
         assertEquals(6,Week3.max(1,6));
     }
+    @Test
     // TODO: Viết 5 testcase cho phương thức minOfArray()
     public void testMin(){
-        int[] array;
-        array[0]=1;
-        array[1]=2;
-        array[2]=3;
+        int[] array = {1,2,3};
         assertEquals(1,Week3.minOfArray(array));
     }
+    @Test
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
-    public void testBMI
-    {
-        double weight;
-        double height;
-        assertEquals("Thieu can",Week3.calculateBMI());
+    public void testBMI(){
+        assertEquals("Thieu can",Week3.calculateBMI(55,1.75));
+        assertEquals("Binh thuong",Week3.calculateBMI(60,1.75));
+        assertEquals("Thua can",Week3.calculateBMI(75,1.75));
+        assertEquals("Beo phi",Week3.calculateBMI(80,1.75));
+        assertEquals("Thieu can",Week3.calculateBMI(55,1.75));
     }
 }
