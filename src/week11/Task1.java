8ce9e2918d28e258eab9f8e22256655ffa912009
17020928 <43131375+17020928@users.noreply.github.com>
2018-11-22 22:36:35 +0700

    change

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,33 @@
 package week11;
 
 public class Task1 {
+    public static <T extends Comparable> void sort(T[] a){
+        if(a==null||a.length==0) return;
+        for(int i = 0 ;i < a.length-1;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i].compareTo(a[j])>0){
+                    T temp;
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] argv){
+        Integer[] word = {5,0,3,4,1};
+        Task1.<Integer>sort(word);
+        for (int i=0;i<word.length;i++)
+        System.out.print(word[i]+" ");
+
+        Short[] word1 = {5,0,3,4,1};
+        Task1.<Short>sort(word1);
+        for (int i=0;i<word1.length;i++)
+            System.out.print(word1[i]+" ");
+
+        String[] word2 = {"Nghia","Nhanh","Thanh giao su","Kha"};
+        Task1.<String>sort(word2);
+        for (int i=0;i<word2.length;i++)
+            System.out.print(word2[i].toString()+" ");
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,22 @@
 package week11;
 
 public class Task2 {
+    public static <T extends Comparable> T max(T[] a){
+        if(a==null||a.length==0) return null;
+        T max=a[0];
+        for(int i=1;i<a.length;i++){
+            if(max.compareTo(a[i])<0) max=a[i];
+        }
+        return max;
+    }
+    public static void main(String[] argv){
+        Integer[] word = {5,0,3,4,1};
+        System.out.print(Task2.<Integer>max(word)+" ");
+
+        Short[] word1 = {5,0,3,4,1};
+        System.out.print(Task2.<Short>max(word1)+" ");
+
+        String[] word2 = {"Nghia","Nhanh","Thanh giao su","Kha"};
+        System.out.print(Task2.<String>max(word2)+" ");
+    }
 }
